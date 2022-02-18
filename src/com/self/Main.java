8f3a319e892184e8ad973f6c0e5a3bb3b2c73fa5
goodl <goodluckajay@gmail.com>
2022-02-18 12:47:23 +0530

    commit into dev branch code

--- src/com/self/Main.java
+++ src/com/self/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	System.out.println("Checking");
         System.out.println("another1");
         System.out.println("another2");
+        System.out.println("This was taken from Development branch and after modifing it, I want to push back to development branch");
 
 
     }
