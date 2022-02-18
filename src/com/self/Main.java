082d578892ea0747db6b53a4664b1848ac9def0f
goodl <goodluckajay@gmail.com>
2022-02-18 13:09:04 +0530

    prod release

--- src/com/self/Main.java
+++ src/com/self/Main.java
@@ -8,6 +8,6 @@ public class Main {
         System.out.println("another2");
         System.out.println("This was taken from Development branch and after modifing it, I want to push back to development branch");
         System.out.println("this was taken from release-to-uat branch and want to commit back in same branch");
-
+        System.out.println("this is production release");
     }
 }
